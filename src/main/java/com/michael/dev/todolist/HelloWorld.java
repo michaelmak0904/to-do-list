acb6afd81692cc77d4e231b80c066685e5e83b1b
Michael Mak <michaelmak0904@gmail.com>
2023-05-14 20:40:57 +0800

    init

---
+++ src/main/java/com/michael/dev/todolist/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.michael.dev.todolist;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/michael/dev/todolist/entity/Todo.java
@@ -0,0 +1,42 @@
+package com.michael.dev.todolist.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column
+    String task;
+
+    @Column
+    Integer status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+}
