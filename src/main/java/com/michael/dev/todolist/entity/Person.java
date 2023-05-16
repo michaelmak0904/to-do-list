dab04089ff74797f23b91cedbdcad475b0f5f62f
Michael MM. MAK <michaelmak@ACSHK.local>
2023-05-16 13:56:43 +0800

    Day 12 - Spring Boot Thymeleaf 小小實作

---
+++ src/main/java/com/michael/dev/todolist/entity/Person.java
@@ -0,0 +1,22 @@
+package com.michael.dev.todolist.entity;
+
+public class Person {
+    String name;
+    String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
